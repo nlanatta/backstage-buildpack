142eda839c652b4b18f05c30582c3539be22a0ab
Chap Ambrose <cambrose@salesforce.com>
2024-05-03 16:04:01 -0500

    reorder profile config

--- bin/support/backstage-startup-config.sh
+++ bin/support/backstage-startup-config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# set a default PGSSLMODE if it does not exist in the environment
-export PGSSLMODE=${PGSSLMODE:-no-verify}
-
+# psql will fail if PGSSLMODE above it
 psql $DATABASE_URL -c \
 "CREATE SCHEMA IF NOT EXISTS auth;
 CREATE SCHEMA IF NOT EXISTS catalog;
@@ -12,3 +10,6 @@ CREATE SCHEMA IF NOT EXISTS techdocs;
 CREATE SCHEMA IF NOT EXISTS proxy;
 CREATE SCHEMA IF NOT EXISTS permission;
 CREATE SCHEMA IF NOT EXISTS app;"
+
+# set a default PGSSLMODE if it does not exist in the environment
+export PGSSLMODE=${PGSSLMODE:-no-verify}
