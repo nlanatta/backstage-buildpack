561f07352831fcab6bd8a65919dbe77abe592e3a
Chap Ambrose <cambrose@salesforce.com>
2024-05-03 15:54:04 -0500

    add startup config

---
+++ bin/support/backstage-startup-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set a default PGSSLMODE if it does not exist in the environment
+export PGSSLMODE=${PGSSLMODE:-no-verify}
+
+psql $DATABASE_URL -c \
+"CREATE SCHEMA IF NOT EXISTS auth;
+CREATE SCHEMA IF NOT EXISTS catalog;
+CREATE SCHEMA IF NOT EXISTS search;
+CREATE SCHEMA IF NOT EXISTS scaffolder;
+CREATE SCHEMA IF NOT EXISTS techdocs;
+CREATE SCHEMA IF NOT EXISTS proxy;
+CREATE SCHEMA IF NOT EXISTS permission;
+CREATE SCHEMA IF NOT EXISTS app;"
