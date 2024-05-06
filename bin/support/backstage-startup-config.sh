3cde06528ed20635f7114bfa558703238a2db0c0
Chap Ambrose <cambrose@salesforce.com>
2024-05-06 16:38:31 -0500

    overwrite baseUrl for review apps

--- bin/support/backstage-startup-config.sh
+++ bin/support/backstage-startup-config.sh
@@ -13,3 +13,8 @@ CREATE SCHEMA IF NOT EXISTS app;"
 
 # set a default PGSSLMODE if it does not exist in the environment
 export PGSSLMODE=${PGSSLMODE:-no-verify}
+
+# override base url for review apps
+if [ "$BASE_URL" = "https://CHANGE-ME.herokuapp.com" ] && [ -n "$REVIEW_APP_BASE_URL_PREFIX" ] && [ -n "$HEROKU_PR_NUMBER" ]; then
+  export APP_CONFIG_backend_baseUrl="https://${REVIEW_APP_BASE_URL_PREFIX}-pr-${HEROKU_PR_NUMBER}.herokuapp.com"
+fi
