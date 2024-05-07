f2df8e19d8578f11e3f5eb19f52ef60c84825546
Chap Ambrose <cambrose@salesforce.com>
2024-05-07 10:13:21 -0500

    tweak base_url overrides

--- bin/support/backstage-startup-config.sh
+++ bin/support/backstage-startup-config.sh
@@ -15,6 +15,8 @@ CREATE SCHEMA IF NOT EXISTS app;"
 export PGSSLMODE=${PGSSLMODE:-no-verify}
 
 # override base url for review apps
-if [ "$BASE_URL" = "https://CHANGE-ME.herokuapp.com" ] && [ -n "$REVIEW_APP_BASE_URL_PREFIX" ] && [ -n "$HEROKU_PR_NUMBER" ]; then
-  export APP_CONFIG_backend_baseUrl="https://${REVIEW_APP_BASE_URL_PREFIX}-pr-${HEROKU_PR_NUMBER}.herokuapp.com"
+if [ "$BASE_URL" = "https://REVIEW-APP.herokuapp.com" ] && [ -n "$REVIEW_APP_BASE_URL_PREFIX" ] && [ -n "$HEROKU_PR_NUMBER" ]; then
+  baseUrl="https://${REVIEW_APP_BASE_URL_PREFIX}-pr-${HEROKU_PR_NUMBER}.herokuapp.com"
+  echo "Overwriting BASE_URL: $baseUrl"
+  export BASE_URL=baseUrl
 fi
