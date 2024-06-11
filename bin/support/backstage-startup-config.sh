49b2217ced34588ac54e5b58daa117c399c745ba
Nicolas Lanatta <nicolaslanatta@gmail.com>
2024-06-11 11:22:36 -0300

    Update backstage-startup-config.sh

--- bin/support/backstage-startup-config.sh
+++ bin/support/backstage-startup-config.sh
@@ -9,7 +9,8 @@ CREATE SCHEMA IF NOT EXISTS scaffolder;
 CREATE SCHEMA IF NOT EXISTS techdocs;
 CREATE SCHEMA IF NOT EXISTS proxy;
 CREATE SCHEMA IF NOT EXISTS permission;
-CREATE SCHEMA IF NOT EXISTS app;"
+CREATE SCHEMA IF NOT EXISTS app;
+CREATE SCHEMA IF NOT EXISTS pagerduty;"
 
 # set a default PGSSLMODE if it does not exist in the environment
 export PGSSLMODE=${PGSSLMODE:-no-verify}
