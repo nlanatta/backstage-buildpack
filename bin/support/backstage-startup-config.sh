55e4b28f3d8f6457ae696db625c651fb66d9daab
Nicolas Lanatta <nicolaslanatta@gmail.com>
2024-06-27 14:35:27 -0300

    Update backstage-startup-config.sh
    Adding kubernetes

--- bin/support/backstage-startup-config.sh
+++ bin/support/backstage-startup-config.sh
@@ -11,7 +11,8 @@ CREATE SCHEMA IF NOT EXISTS proxy;
 CREATE SCHEMA IF NOT EXISTS permission;
 CREATE SCHEMA IF NOT EXISTS app;
 CREATE SCHEMA IF NOT EXISTS pagerduty;
-CREATE SCHEMA IF NOT EXISTS webhooks;"
+CREATE SCHEMA IF NOT EXISTS webhooks;
+CREATE SCHEMA IF NOT EXISTS kubernetes;"
 
 # set a default PGSSLMODE if it does not exist in the environment
 export PGSSLMODE=${PGSSLMODE:-no-verify}
